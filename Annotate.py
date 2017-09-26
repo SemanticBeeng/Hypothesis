3e6bbb8f335a731085ad4b3945d2b47461dddada
SemanticBeeng <nick@semanticbeeng.com>
2017-09-25 23:10:16 -0700

    making programmatic annotations

---
+++ Annotate.py
@@ -0,0 +1,47 @@
+import hypothesis
+
+
+def make_annotation(username: str, token: str) -> None:
+    h = hypothesis.Hypothesis(username, token)  # your h username and api token (from https://hypothes.is/account/developer)
+
+    url = 'https://github.com/SemanticBeeng/Hypothesis'
+    exact = 'annotation'
+    prefix = ''
+    suffix = ''
+    title = 'title of the web page'
+    tags = ["tag1", "tag2"]
+    text = "found it"
+
+    payload = {
+        "uri": url,
+        "target":
+            [{
+                "source": [url],
+                "selector":
+                    [{
+                        "type": "TextQuoteSelector",
+                        "prefix": prefix,
+                        "exact": exact,
+                        "suffix": suffix
+                    }
+                    ]
+            }],
+        "tags": tags,
+        "text": text,
+        "document": {
+            "title": [title]
+        },
+        "permissions": h.permissions,
+        "group": h.group
+    }
+
+    r = h.post_annotation(payload)
+    print(r.status_code)
+
+
+def main():
+    make_annotation("nick@domainspecificconsulting.com", "6879-kqqV-XH4sr4nWjozKuohjb7bwKVBjzQhGVtptcjwsuk")
+
+
+if __name__ == "__main__":
+    main()
